37ca85593f8c26b6ae79c2aadd2a6ebc901e8fb9
Andy0414 <pjh8667@naver.com>
2019-06-27 10:00:49 +0900

    UPDATE user model

---
+++ model/User.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var UserSchema = new Schema({
+    username: String, // 유저 이름
+    email: String, // 이메일
+    password: String, // 패스워드
+    registerTime: { // 가입 시간
+        type: Date,
+        default: Date.now
+    },
+    lastLogin: { // 최근 로그인 시간
+        type: Date,
+        default: Date.now
+    },
+    isAdmin: Boolean // 어드민 권한
+})
+
+/**
+ * @description 아이디로 유저를 찾습니다.
+ * @param {*} id 유저 아이디
+ * @param {*} callback (err,data)=>{}
+ */
+UserSchema.statics.findUserById = function (id, callback) {
+    return this.find({ id: id }, callback)
+}
+module.exports = mongoose.model('User', UserSchema);
--- router/auth/auth.controllter.js
+++ router/auth/auth.controllter.js
@@ -1,3 +1,11 @@
-module.exports.createUser = function(req,res){
-    
+
+module.exports = {
+    /**
+     * @description 유저를 생성합니다.
+     *
+     * @param {*} req
+     * @param {*} res
+     */
+    createUser(req, res) {
+    }
 }
